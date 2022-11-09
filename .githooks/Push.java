79a1b4b093e3212807435674cfe8590a8b3b5b54
Antonin SIMON <antonin.simon@epsi.fr>
2022-11-09 11:55:26 +0100

    Push hook creation

--- .githooks/Push.java
+++ .githooks/Push.java
@@ -1,7 +1,22 @@
 import java.io.IOException;
+import java.util.Scanner; 
 
 public class Push {
     public static void main(String[] args) throws IOException{
-        System.exit(0);
+        System.out.println("Vous allez push sur la branche " + args[1]);
+        
+        Scanner myObj = new Scanner(System.in);  // Create a Scanner object
+        System.out.println("Validez-vous l'opération ?(y/n)");
+
+        String answer = myObj.nextLine();  // Read user input
+
+        System.out.println("Votre réponse : " + answer);
+
+        if(answer == "yes" || answer == "y" || answer == ""){
+            System.out.println("Push réussi");
+            System.exit(1);
+        } else {
+            System.exit(1);
+        }
     }
 }
