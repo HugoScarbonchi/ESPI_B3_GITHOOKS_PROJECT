48c02d4713381633872361630d1e08fd8b6b8fb2
Maxime <maxime.baudoin@epsi.fr>
2022-11-23 20:18:15 +0100

    Méthodes de Produit

--- src/project/Product.java
+++ src/project/Product.java
@@ -3,5 +3,44 @@ public class Product {
     String name;
     Collection collection;
     long price;
+
+    public Product(String name) {
+
+        this.name = name;
+    
+    }
+    
+    public Product(String name, long price) {
+    
+        this.name = name;
+        this.price = price;
+    
+    }
+    
+    public Product(String name, long price, Collection collection) {
+    
+        this.name = name;
+        this.price = price;
+        this.collection = collection;
+    
+    }
+    
+    public void ajouterPromotion(long promo) {
+        
+        this.price = this.price - promo;
+    
+    }
+    
+    public void ajouterPromotion(float pourcentage) {
+    
+        this.price = this.price - this.price * pourcentage;
+    
+    }
+    
+    public void setCollection(Collection collection) {
+    
+        this.collection = collection;
+    
+    }
     
 }
