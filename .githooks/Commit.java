60c9a5365eb35c52143bbe8ce1f82ddaec73db35
Hugo Scarbonchi <hugo.scarbonchi@mink-agency.com>
2022-11-28 15:50:45 +0100

    un message

---
+++ .githooks/Commit.java
@@ -0,0 +1,23 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.time.format.DateTimeFormatter;
+import java.time.LocalDateTime;
+
+public class Commit {
+    public static void main(String[] args) throws IOException{
+        String data = "";
+
+        data = new String(Files.readAllBytes(Paths.get(args[0])));
+        data = data.trim();
+
+        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm:ss");
+        LocalDateTime now = LocalDateTime.now();
+        currentDate = dtf.format(now);
+
+        Systeme.out.println(currentDate);
+
+
+        System.exit(1);
+    }
+}
